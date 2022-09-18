faf7020e45ceb85d7240e979198e50dcdbd3b4c2
frodriguez84 <fernando.rodriguez84@yahoo.com.ar>
2022-09-18 12:57:03 -0300

    Projectile

--- js/classes/Projectile.js
+++ js/classes/Projectile.js
@@ -1,6 +1,6 @@
-class Projectile {
+class Projectile extends Sprite{
     constructor({ position = { x: 0, y: 0 }, enemy }) {
-        this.position = position
+        super({ position })
         this.velocity = {
             x: 0,
             y: 0
@@ -9,12 +9,7 @@ class Projectile {
         this.radius = 10
         this.power = 5
     }
-    draw() {
-        c.beginPath()
-        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
-        c.fillStyle = 'orange'
-        c.fill()
-    }
+   
     update() {
         this.draw()
         this.shootProjectile()
