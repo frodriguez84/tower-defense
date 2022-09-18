04fc25df3b773b4d28794bee953eab92fecaa7ba
frodriguez84 <fernando.rodriguez84@yahoo.com.ar>
2022-09-18 13:17:57 -0300

    Enemy sprite

--- js/classes/Sprite.js
+++ js/classes/Sprite.js
@@ -1,12 +1,44 @@
 class Sprite {
-    constructor({ position = { x: 0, y: 0 } }) {
+    constructor({ position = { x: 0, y: 0 }, imgSrc, frames = { max: 1 } }) {
         this.position = position
         this.image = new Image()
-        this.image.src = 'img/projectile.png'
+        this.image.src = imgSrc
+        this.frames = {
+            max: frames.max,
+            current: 0,
+            elapsed: 0,
+            hold: 4
+        }
     }
 
     draw() {
-        c.drawImage(this.image, this.position.x, this.position.y)
-
+        const cropWidth = this.image.width / this.frames.max
+        const crop = {
+            position: {
+                x: cropWidth * this.frames.current,
+                y: 0
+            },
+            width: cropWidth,
+            height: this.image.height
+        }
+        c.drawImage(
+            this.image,
+            crop.position.x,
+            crop.position.y,
+            crop.width,
+            crop.height,
+            this.position.x,
+            this.position.y,
+            crop.width,
+            crop.height
+        )
+        //Responsible for animation
+        this.frames.elapsed++
+        if (this.frames.elapsed % this.frames.hold === 0) {
+            this.frames.current++
+            if (this.frames.current >= this.frames.max - 1) {
+                this.frames.current = 0
+            }
+        }
     }
 }
